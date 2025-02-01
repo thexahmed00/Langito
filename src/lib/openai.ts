82199359739a851c1449bcf2f8c8954cc79913d7
themxahmed00 <ma2k510@gmail.com>
2025-02-01 21:41:33 +0530

    removde openai key

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,10 +1,6 @@
-import OpenAI from "openai"
+
 import axios from "axios"
 
-// const openai = new OpenAI({
-//   apiKey: "2g5pUCgt8zoCa9Q0dKOdqjXbD4SAsNQJUhJ3Vgsg46gSN2CqXUFXJQQJ99BBACYeBjFXJ3w3AAABACOGx2h9",
-//   dangerouslyAllowBrowser: true,
-// })
 
 export interface ChatMessage {
   role: 'system' | 'user' | 'assistant'
@@ -37,20 +33,5 @@ console.log(import.meta.env.VITE_AZURE_OPENAI_ENDPOINT);
     console.error("Error fetching OpenAI response:", error)
     throw error
   }
-  
-  // try {
-  //   const response = await openai.chat.completions.create({
-  //       model: "gpt-3.5-turbo",
-  //       messages: prompt,
-  //       max_tokens: 150,
-  //       temperature: 0.6, // Controls randomness (0-1)
-  //       presence_penalty: 0.6, // Encourages new topics
-  //       frequency_penalty: 0.5 // Reduces repetition
-  //     });
-  //     const content = response.choices[0].message.content;
-  //     return content
-  // } catch (error) {
-  //   console.error("Error fetching OpenAI response:", error)
-  //   throw error
-  // }
+
 }
